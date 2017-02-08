772fb21e043258a38e78b2091c09a15d88c06adf
sudoes <hafiz.jefri@gmail.com>
2017-02-08 12:48:55 +0800

    add helper method

--- src/com/xyz/crms/controller/manager/CarManager.java
+++ src/com/xyz/crms/controller/manager/CarManager.java
@@ -5,11 +5,51 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.xyz.crms.model.Car;
 
 public class CarManager {
+	
+	// Helper methods
+
+	private void writeCar(Car car, PreparedStatement ps) throws SQLException {
+
+		ps.setString(1, car.getPlateNol());
+		ps.setString(2, car.getModel());
+		ps.setDouble(3, car.getPrice());
+		ps.setString(4, Character.toString(car.getStatus()));
+
+		if (car.getCarID() != 0) {
+			ps.setInt(5, car.getCarID());
+		}
+	}
+
+	private ArrayList<Car> searchCars(PreparedStatement ps) throws SQLException {
+
+		ArrayList<Car> cars = new ArrayList<>();
+		ResultSet rs = ps.executeQuery();
+
+		while (rs.next()) {
+
+			Car car = new Car();
+
+			car.setCarID(rs.getInt("CarID"));
+			car.setPlateNol(rs.getString("PlateNo"));
+			car.setModel(rs.getString("Model"));
+			car.setPrice(rs.getDouble("Price"));
+			car.setStatus(rs.getString("Status").charAt(0));
+
+			cars.add(car);
+		}
+		
+		rs.close();
+		return cars;
+	}
+	
+	// End helper methods
 
 	public int addCar(Car car) throws ClassNotFoundException, SQLException{
 
@@ -22,21 +62,19 @@ public class CarManager {
 		PreparedStatement ps = connection.prepareStatement("INSERT INTO Car (PlateNo, Model, Price, Status) VALUES (?, ?, ?, ?)",
 				PreparedStatement.RETURN_GENERATED_KEYS);
 
-		ps.setString(1, car.getPlateNol());
-		ps.setString(2, car.getModel());
-		ps.setDouble(3, car.getPrice());
-		ps.setString(4, Character.toString(car.getStatus()));
+		writeCar(car, ps);
+
 
 		int status = ps.executeUpdate();
 		connection.close();
 
-		//		if (status != 0) {
-		//			ResultSet rs = ps.getGeneratedKeys();
-		//			
-		//			if (rs.next()) {
-		//				car.setCarID(rs.getInt(1));
-		//			}
-		//		}
+		if (status != 0) {
+			ResultSet rs = ps.getGeneratedKeys();
+
+			if (rs.next()) {
+				car.setCarID(rs.getInt(1));
+			}
+		}
 
 		return status;
 	}
@@ -52,11 +90,7 @@ public class CarManager {
 		PreparedStatement ps = connection.prepareStatement("UPDATE Car SET PlateNo=?, Model=?, Price=?, Status=? WHERE CarID=?",
 				PreparedStatement.RETURN_GENERATED_KEYS);
 
-		ps.setString(1, car.getPlateNol());
-		ps.setString(2, car.getModel());
-		ps.setDouble(3, car.getPrice());
-		ps.setString(4, Character.toString(car.getStatus()));
-		ps.setInt(5, car.getCarID());
+		writeCar(car, ps);
 
 		int status = ps.executeUpdate();
 		connection.close();
@@ -92,12 +126,9 @@ public class CarManager {
 		return status;
 	}
 
+
 	public ArrayList<Car> searchCars(String keyword, int type) throws ClassNotFoundException, SQLException{
-		
-		// Initialize array 
-		ArrayList<Car> cars = new ArrayList<>();
-		
-		
+
 		// Load DB Driver
 		Class.forName("org.apache.derby.jdbc.ClientDriver");
 
@@ -105,29 +136,61 @@ public class CarManager {
 
 		// Create SQL Statement
 		PreparedStatement ps = null;
-		
+
 		// Ternary operator
 		ps = connection.prepareStatement("SELECT * FROM Car WHERE UPPER(" + (type == 0 ? "PlateNo" : "Model") + ") LIKE ?",
 				PreparedStatement.RETURN_GENERATED_KEYS) ;
-		
+
 		ps.setString(1, "%" + keyword.toUpperCase() + "%");
 
-		ResultSet rs = ps.executeQuery();
-		
-		while (rs.next()) {
-			
-			Car car = new Car();
-			
-			car.setCarID(rs.getInt("CarID"));
-			car.setPlateNol(rs.getString("PlateNo"));
-			car.setModel(rs.getString("Model"));
-			car.setPrice(rs.getDouble("Price"));
-			car.setStatus(rs.getString("Status").charAt(0));
-			
-			cars.add(car);
+		ArrayList<Car> cars = searchCars(ps);
+
+		connection.close();
+
+		return cars;
+	}
+
+	public ArrayList<Car> searchCars(double price, int type) throws ClassNotFoundException, SQLException{
+
+		// Load DB Driver
+		Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+		Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/dbCRMS", "user", "123");
+
+		// Create SQL Statement
+		PreparedStatement ps = null;
+
+
+		if (type == 0) {
+			ps = connection.prepareStatement("SELECT * FROM Car WHERE Price <= ?", PreparedStatement.RETURN_GENERATED_KEYS);
+		} else if (type == 1) {
+			ps = connection.prepareStatement("SELECT * FROM Car WHERE Price = ?", PreparedStatement.RETURN_GENERATED_KEYS);
+		} else {
+			ps = connection.prepareStatement("SELECT * FROM Car WHERE Price >= ?", PreparedStatement.RETURN_GENERATED_KEYS);
 		}
-		
-		rs.close();
+
+		ps.setDouble(1, price);
+		ArrayList<Car> cars = searchCars(ps);
+		connection.close();
+
+		return cars;
+	}
+
+	public ArrayList<Car> searchCars(Date start) throws ClassNotFoundException, SQLException{
+
+		// Load DB Driver
+		Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+		Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/dbCRMS", "user", "123");
+
+		// Create SQL Statement
+		PreparedStatement ps = connection.prepareStatement("SELECT * FROM Car WHERE CarID NOT IN (SELECT CarID FROM Rental WHERE ? BETWEEN"
+				+ " Start AND {fn TIMESTAMPADD(SQL_TSI_MINUTE, Duration * 60 - 1, Start)} AND Status = 'A'");
+
+		// Convert Date to timestamp
+		ps.setTimestamp(1, new Timestamp(start.getTime()));
+
+		ArrayList<Car> cars = searchCars(ps);
 		connection.close();
 
 		return cars;
