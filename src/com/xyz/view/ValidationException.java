6a0d0f3fa071724aecd59de3345db8705143d63c
sudoes <hafiz.jefri@gmail.com>
2017-02-10 17:18:55 +0800

    refactor validation exception

---
+++ src/com/xyz/view/ValidationException.java
@@ -0,0 +1,10 @@
+package com.xyz.view;
+
+public class ValidationException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(String message) {
+		super(message);
+	}
+
+}
